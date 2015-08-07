22ea22c99e09fa1d2850896b9608a3a72a0ccc53
Dan Leehr <dan.leehr@duke.edu>
2015-08-06 20:51:34 -0400

    Initial Commit

---
+++ PlayPen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlayPen
+//
+//  Created by Dan Leehr on 8/6/15.
+//  Copyright (c) 2015 Dan Leehr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlayPen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlayPen
+//
+//  Created by Dan Leehr on 8/6/15.
+//  Copyright (c) 2015 Dan Leehr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
